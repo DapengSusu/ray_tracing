4d7332319734a11f66b86aff6fb76c4f0eef5035
Jumper <dapengsusu@qq.com>
2025-01-04 21:27:25 +0800

    Perlin texture with trilinear interpolation

--- the_next_week/src/perlin.rs
+++ the_next_week/src/perlin.rs
@@ -2,13 +2,14 @@ use utils::rtweekend::{random, random_range};
 use vector3::Point3;
 
 const POINT_COUNT: usize = 256;
-type Perm = [usize; POINT_COUNT];
+type PermArr = [usize; POINT_COUNT];
+type TriArr = [[[f64; 2]; 2]; 2];
 
 pub struct Perlin {
     randfloat: [f64; POINT_COUNT],
-    perm_x: Perm,
-    perm_y: Perm,
-    perm_z: Perm
+    perm_x: PermArr,
+    perm_y: PermArr,
+    perm_z: PermArr
 }
 
 impl Perlin {
@@ -27,26 +28,59 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.*p.x) as usize & 255;
-        let j = (4.*p.y) as usize & 255;
-        let k = (4.*p.z) as usize & 255;
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
 
-        self.randfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let mut c: TriArr = [[[0.; 2]; 2]; 2];
+        let i = p.x.floor() as usize;
+        let j = p.y.floor() as usize;
+        let k = p.z.floor() as usize;
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    c[di][dj][dk] = self.randfloat[
+                        self.perm_x[(i+di) & 255] ^
+                        self.perm_y[(j+dj) & 255] ^
+                        self.perm_z[(k+dk) & 255]
+                    ];
+                }
+            }
+        }
+
+        Self::trilinear_interp(&c, u, v, w)
     }
 
-    fn perlin_generate_perm(perm: &mut Perm) {
+    fn perlin_generate_perm(perm: &mut PermArr) {
         for i in 0..POINT_COUNT {
             perm[i] = i;
         }
         Self::permute(perm, POINT_COUNT);
     }
 
-    fn permute(perm: &mut Perm, n: usize) {
+    fn permute(perm: &mut PermArr, n: usize) {
         for i in (1..n).rev() {
             let target = random_range::<usize>(0, i);
 
             perm.swap(i, target);
         }
+    }
+
+    fn trilinear_interp(c: &TriArr, u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.;
+
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1-i) as f64 * (1.-u))
+                           * (j as f64 * v + (1-j) as f64 * (1.-v))
+                           * (k as f64 * w + (1-k) as f64 * (1.-w))
+                           * c[i][j][k];
+                }
+            }
+        }
+
+        accum
     }
 }
 
